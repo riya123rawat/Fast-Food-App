9eadde9df7e8e81fff010127daadc38a80da5807
riya123rawat <163911743+riya123rawat@users.noreply.github.com>
2024-08-26 15:08:36 +0200

    Update ThemeToggle.jsx
    add style

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 const ThemeToggle = ({ toggleTheme, isDarkTheme }) => {
   return (
-    <button onClick={toggleTheme}>
+    <button onClick={toggleTheme}style={{ backgroundColor: 'red', color: 'white' }}>
       Switch to {isDarkTheme ? 'Light' : 'Dark'} Theme
     </button>
   );
